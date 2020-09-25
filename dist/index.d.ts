bfa94f2749324029ed4b4c51dfaf3e27bafe3eed
Jim Gustafson <jim@signal.org>
2020-09-25 14:43:22 -0700

    Update to RingRTC v2.7.2

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,4 +1,4 @@
-export { AudioDevice, Call, CallId, CallEndedReason, CallLogLevel, CallState, CallingMessage, CallSettings, DeviceId, RingRTCType, UserId, VideoCapturer, VideoRenderer } from './ringrtc/Service';
+export { AudioDevice, Call, CallId, CallEndedReason, CallLogLevel, CallState, CallingMessage, CallSettings, DeviceId, HangupMessage, HangupType, RingRTCType, OfferType, UserId, VideoCapturer, VideoRenderer } from './ringrtc/Service';
 export { CanvasVideoRenderer, GumVideoCapturer } from './ringrtc/VideoSupport';
 import { RingRTCType } from './ringrtc/Service';
 export declare const RingRTC: RingRTCType;
